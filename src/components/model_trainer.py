d1722c1538af2406537301a17448c8116ca1db77
manvith-g <manvithgowda.k31@gmail.com>
2026-08-19 13:56:59 +0530

    Hyper Paramete Tuning: Implemented in modular coding

--- src/components/model_trainer.py
+++ src/components/model_trainer.py
@@ -41,14 +41,54 @@ class ModelTrainer:
                 "Random Forest":RandomForestRegressor(),
                 "Decision Tree":DecisionTreeRegressor(),
                 "Gradient Boosting": GradientBoostingRegressor(),
-                "Liner Regressor": LinearRegression(),
+                "Linear Regressor": LinearRegression(),
                 "K-Neighbour Regressor": KNeighborsRegressor(),
                 "xg-boost Regressor":XGBRegressor(),
                 'CatBoosting Regressor':CatBoostRegressor(verbose=False),
                 'AdaBoost Regressor':AdaBoostRegressor()
             }
+            params={
+                "Decision Tree":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    'splitter':['best','random'],
+                    'max_features':['sqrt','log2']
+                },
+                "Random Forest":{
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                "Gradient Boosting":{
+                    'loss':['squared_error','huber','absolute_error','poisson'],
+                    'learning_rate':[0.6,0.7,0.75,0.8,0.85,0.9],
+                    'criterion':['squared_error','friedman_mse','absolute_error','poisson'],
+                    'max_features':['sqrt','log2',None],
+                    'n_estimators':[8,16,32,64,128,256]
+
+                },
+                "Linear Regressor":{},
+                "K-Neighbour Regressor":{
+                    'n_neighbors':[5,7,9,11],
+                    'weights':['uniform','distance'],
+                    'algorithm':['ball_tree','kd_tree','brute']
+                },
+                "xg-boost Regressor":{
+                    'learning_rate':[0.1,0.01,0.05,0.001],
+                    'n_estimators':[8,16,32,64,128,256]
+                },
+                'CatBoosting Regressor':{
+                    'depth':[6,8,10],
+                    'learning_rate':[0.01,0.05,0.1],
+                    'iterations':[30,50,100]
+                },
+                 'AdaBoost Regressor':{
+                     'learning_rate':[0.1,0.01,0.05,0.001],
+                     'loss':['linear','square','exponential'],
+                    'n_estimators':[8,16,32,64,128,256]
+                 }
+            }
             logging.info("Model evalutaion function called...")
-            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models) #defination in the utils # reference of model is passing here
+            model_report:dict=evaluate_models(X_train,y_train,X_test,y_test,models,params) #defination in the utils # reference of model is passing here
 
             best_model_score=max(sorted(model_report.values()))
 
--- src/utils.py
+++ src/utils.py
@@ -3,6 +3,7 @@ import numpy as np
 import pandas as pd
 from src.exception import CustomException
 from src.logger import logging
+from sklearn.model_selection import GridSearchCV
 
 from sklearn.metrics import r2_score
 
@@ -16,17 +17,24 @@ def save_object(file_path,obj):
             dill.dump(obj,file_obj)
     except Exception as e:
         raise CustomException(e,sys)
+    
 
 
-def evaluate_models(X_train,y_train,X_test,y_test,models):
+def evaluate_models(X_train,y_train,X_test,y_test,models,params): # with params(hyperparameter tuning)
     try:
         logging.info("Evaluation started")
         report={}
 
         for i in range(len(list(models))):
             logging.info(f"{list(models.keys())[i]} Started")
-            model=list(models.values())[i] # model is a refernce of AnyModel() insted of doinf like randomforest.fit() we are doing model.fit but insdie memoty it is saving in same place before it was not trained but after trained
+            model=list(models.values())[i]                        # model is a refernce of AnyModel() insted of doinf like randomforest.fit() we are doing model.fit but insdie memoty it is saving in same place before it was not trained but after trained
+            param=params[list(models.keys())[i]]
 
+
+            grid=GridSearchCV(model,param,cv=5,n_jobs=-1) #other parameter u can give :    n_jobs=  ,  verbose=  ,  refit=   , etc 
+            grid.fit(X_train,y_train)
+
+            model.set_params(**grid.best_params_)
             model.fit(X_train,y_train)
 
             y_train_pred=model.predict(X_train)
@@ -43,4 +51,33 @@ def evaluate_models(X_train,y_train,X_test,y_test,models):
 
     except Exception as e:
         raise CustomException(e,sys)
+
+
+
+
+# def evaluate_models(X_train,y_train,X_test,y_test,models): # without params
+#     try:
+#         logging.info("Evaluation started")
+#         report={}
+
+#         for i in range(len(list(models))):
+#             logging.info(f"{list(models.keys())[i]} Started")
+#             model=list(models.values())[i] # model is a refernce of AnyModel() insted of doinf like randomforest.fit() we are doing model.fit but insdie memoty it is saving in same place before it was not trained but after trained
+
+#             model.fit(X_train,y_train)
+
+#             y_train_pred=model.predict(X_train)
+#             y_test_pred=model.predict(X_test)
+
+#             train_model_score=r2_score(y_train,y_train_pred) # do something with this tooooooooo later
+#             test_model_score=r2_score(y_test,y_test_pred)
+
+#             report[list(models.keys())[i]]= test_model_score
+#             logging.info(f"{list(models.keys())[i]} Ended | R2_SCORE : {test_model_score}")
+
+#         logging.info(f"Evaluation Ended | Model Trained succefully ")
+#         return report
+
+#     except Exception as e:
+#         raise CustomException(e,sys)
     
